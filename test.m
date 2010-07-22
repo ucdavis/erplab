8b3427991f9cef4adcd98c212a195953fd5b454b
stanzsax <stanzsax@3d69cea9-74d3-4761-8fae-dae0bb05a87c>
2010-07-22 23:37:47 +0000

    Test

--- test.m
+++ test.m
@@ -2,9 +2,13 @@
 % and a third comment
 % and a 4th
 % and a 5th
+% test
 % and a 6th
 % i am 12 and what is this?
 % this is my friend helping me learn svn the quick way!
-% Signed ~ your friendly neighbor, Stan
 
 % Javier is just learning right now ~ from Stan
+
+%test
+
+%test
