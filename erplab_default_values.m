1a8e48147ce919d9634b121565ca7b24d227e4b5
javlopez <javlopez@3d69cea9-74d3-4761-8fae-dae0bb05a87c>
2013-10-17 01:35:40 +0000

    version number was change to 4.0.0.12

--- erplab_default_values.m
+++ erplab_default_values.m
@@ -1,10 +1,11 @@
-erplabver = '4.0.0.11';           % current erplab version
-erplabrel = '07-Oct-2013';        % DOB
-%ColorB = [170 180 195]/255;      % old background color (until version 3)
-%ColorB = [0.9216 0.8353 0.6078]; % background color for version 4
+erplabver = '4.0.0.12';            % current erplab version
+erplabrel = '16-Oct-2013';         % DOB
+%ColorB = [170 180 195]/255;       % old background color (until version 3)
+%ColorB = [0.9216 0.8353 0.6078];  % background color for version 4
 %ColorB = [0.7020 0.7647 0.8392];  % background color since version 4.0.0.3
-ColorB = [0.9804 0.8118 0.5529];  % background color since version 4.0.0.11
-ColorF = [0.0078 0.0078 0.0078];
+%ColorB = [0.9804 0.8118 0.5529];  % background color since version 4.0.0.11
+ColorB = [0.7020 0.77 0.85];      % background color since version 4.0.0.12
+ColorF = [0.02 0.02 0.02];
 fontsizeGUI = 8.8;
 if ispc
     fontunitsGUI = 'pixels';
