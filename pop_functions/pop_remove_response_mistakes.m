9bd2a6c34cd53bee5a59a36ac50736e8a2ef9ea0
andrewxstewart <5137405+andrewxstewart@users.noreply.github.com>
2020-01-15 17:01:16 -0800

    Remove Response Mistakes help
    Now with fewer typos and better help comments

--- pop_functions/pop_remove_response_mistakes.m
+++ pop_functions/pop_remove_response_mistakes.m
@@ -5,7 +5,7 @@
 % axs Jan 2020
 function [ALLEEG,EEG, hist_com] = pop_remove_response_mistakes(ALLEEG,EEG,CURRENTSET,stim_codes,resp_codes)
 
-% check input
+% check input, call GUI iff not specified
 if exist('stim_codes','var') == 0 || isempty(stim_codes)
     disp('Starting Remove Response Mistakes GUI to get info')
     [stim_codes, resp_codes] = gui_remove_response_mistakes(EEG);
